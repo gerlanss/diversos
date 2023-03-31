24ef26cb19472e8c6614d81a9483db47ed31ce88
gerlanss <gerlan.ss@hotmail.com>
2023-03-31 07:13:59 -0400

    Update EntregaBee.py

--- EntregaBee.py
+++ EntregaBee.py
@@ -5,6 +5,7 @@ import ctypes
 import sys
 import fitz
 import pdfplumber
+from PyQt5 import QtWidgets
 from PyQt5.QtPrintSupport import QPageSetupDialog
 from PyQt5.QtGui import QPdfWriter
 from PyPDF2 import PdfReader
@@ -224,7 +225,7 @@ class EntregaBee(QMainWindow):
         if os.path.exists(fileName):
             # Abrir o arquivo PDF no visualizador
             doc = fitz.open(fileName)
-            pdf_viewer = PdfViewerWindow(self)
+            pdf_viewer = PdfViewerWindow()
             pdf_viewer.view_pdf(doc)
             pdf_viewer.show()
             doc.close()
@@ -298,9 +299,8 @@ class EntregaBee(QMainWindow):
         self.forma_pag_edit.clear()
         self.obs_edit.clear()
 
-class PdfViewerWindow(QMainWindow):
+class PdfViewerWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
-        super().__init__(parent)
         self.setWindowTitle("ENTREGA BEE")
         self.label = QLabel(self)
 
